b120af78789c3c0b76b1d32cfa25b4165b94796d
er <er@Erics-MacBook-Pro-M1.local>
2021-08-09 21:46:57 -0700

    change logo

--- js/packages/web/src/components/Notifications/index.tsx
+++ js/packages/web/src/components/Notifications/index.tsx
@@ -482,7 +482,7 @@ export function Notifications() {
       content={content}
       trigger="click"
     >
-      <h1 className="title">M</h1>
+      <h1 className="title">RADIATE</h1>
     </Popover>
   );
 
